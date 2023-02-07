19e3653759bfd8ff03c101ca98aaed8d11f596b6
guillermo rodriguez <guillermordgz.96@gmail.com>
2023-02-06 19:29:18 -0800

    fix

--- index.js
+++ index.js
@@ -2,14 +2,36 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 
-app.use('/places', require('./controllers/places'))
+
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine ())
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+
+app.use('/places',require('./controllers/places'))
+
 
 app.get('/', (req, res) => {
-    res.send('Hello world!')
+    res.render('home')
 })
 
-app.get('*', (req, res) => {
-    res.status(404).send('<h1>404 Page</h1>')
+
+app.get('*', (req,res) => {
+    res.render('error404')
 })
+
+
+app.get('/', (req, res) => {
+    res.render('new_form')
+})
+
+
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
 
 app.listen(process.env.PORT)
+
+console.log('hungry')
