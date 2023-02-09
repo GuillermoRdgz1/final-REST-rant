96dc0aa16db2e7ff80f14fc85f35db1bea6991f7
guillermo rodriguez <guillermordgz.96@gmail.com>
2023-02-08 17:16:30 -0800

    index fix

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -5,7 +5,9 @@ function show (data) {
     return (
         <Def>
           <main>
+          
             <h1>{ data.place.name }</h1>
+            <p>{data.place.city}, {data.place.state}</p>
             <img id="show" src={data.place.pic} alt={data.place.name}/>
             <h2>Rating</h2>
             <p>Not Rated</p>
@@ -13,16 +15,16 @@ function show (data) {
                 <h2>Comments</h2>
                     <p>No comments yet!</p>
             </div>
-            <a href={`/places/${data.id}/edit`} className="btn btn-primary"> 
+            <h5>Serving {data.place.cuisines}</h5>
+            <a href={`/places/${data.id}/edit`} className="btn btn-warning"> 
               Edit
             </a>  
-<form method="POST" action={`/places/${data.id}?_method=DELETE`}> 
-  <button type="submit" className="btn btn-danger">
-    Delete
-  </button>
-</form> 
-
-
+              <form method="POST" action={`/places/${data.id}?_method=DELETE`}> 
+                <button type="submit" className="btn btn-danger">
+                   Delete
+                </button>
+              </form> 
+          
           </main>
         </Def>
         
